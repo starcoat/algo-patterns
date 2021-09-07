97ec19921613af33e3e4e90f538a27497387f687
Christian Zieske <me@ekseiz.com>
2021-09-07 21:44:50 +0200

    Sort array by descending numbers

--- second_max.php
+++ second_max.php
@@ -2,3 +2,30 @@
 <?php
 
 $numbers = array_slice($argv, 1);
+
+$max_numbers = array();
+$max_numbers[] = array(
+	'number' => array_shift($numbers),
+	'count'  => 1
+);
+
+foreach ($numbers as $n) {
+
+	foreach ($max_numbers as $mn) {
+
+		if ($n > $mn['number']) {
+			array_unshift($max_numbers, array(
+				'number' => $n,
+				'count'  => 1
+			));
+			if (count($max_numbers) > 2) {
+				array_pop($max_numbers);
+			}
+			break;
+		}
+
+	}
+
+}
+
+print_r($max_numbers);
