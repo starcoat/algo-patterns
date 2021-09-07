51e7a86c73269214d07de33d3fed4176fb7b5837
Christian Zieske <me@ekseiz.com>
2021-09-07 23:25:37 +0200

    Insert new number into max_numbers at correct position; Concat output

--- second_max.php
+++ second_max.php
@@ -1,7 +1,12 @@
 #!/usr/bin/php
 <?php
 
+// Rollen von Variablen?
+
 $numbers = array_slice($argv, 1);
+$numbers = array(1,3,11,6,11,5,11,34);
+
+$nth_max = 2;
 
 $max_numbers = array();
 $max_numbers[] = array(
@@ -11,21 +16,53 @@ $max_numbers[] = array(
 
 foreach ($numbers as $n) {
 
-	foreach ($max_numbers as $mn) {
+	$i = 0;
+
+	foreach ($max_numbers as &$mn) {
 
 		if ($n > $mn['number']) {
-			array_unshift($max_numbers, array(
-				'number' => $n,
-				'count'  => 1
-			));
-			if (count($max_numbers) > 2) {
+
+			$max_numbers = array_merge(
+				array_slice($max_numbers, 0, $i),
+				array(array(
+					'number' => $n,
+					'count'  => 1
+				)),
+				array_slice($max_numbers, $i)
+			);
+
+			if (count($max_numbers) > $nth_max) {
 				array_pop($max_numbers);
 			}
 			break;
+
+		} elseif ($n == $mn['number']) {
+			$mn['count']++;
+			break;
 		}
+
+		$i++;
 
 	}
 
 }
 
-print_r($max_numbers);
+
+if ($nth_max == 1) {
+	$nth_string = '';
+} elseif (substr($nth_max, -2) == '01') {
+	$nth_string = $nth_max . 'st ';
+} elseif (($nth_max == 2) OR (substr($nth_max, -2) == '02')) {
+	$nth_string = $nth_max . 'nd ';
+} elseif (($nth_max == 3) OR (substr($nth_max, -2) == '03')) {
+	$nth_string = $nth_max . 'rd ';
+} else {
+	$nth_string = $nth_max . 'th ';
+}
+
+printf("The %slargest number %d appeared %d %s.\n",
+	$nth_string,
+	$max_numbers[$nth_max - 1]['number'],
+	$max_numbers[$nth_max - 1]['count'],
+	($max_numbers[$nth_max - 1]['count'] == 1) ? 'time' : 'times'
+);
