4e8ad2b9754e10dae3c92f799204ac471e3bc23d
Christian Zieske <me@ekseiz.com>
2021-09-09 23:41:02 +0200

    Implement zigzag logic

--- longest_zigzag.php
+++ longest_zigzag.php
@@ -4,8 +4,61 @@
 $numbers = array_slice($argv, 1);
 
 
-$length = 0;
+$cur_length = 0;
+$max_length = 0;
+$next = 'ANY'; // 'ANY' | 'GOL' (Greater or Lower) | 'GTR' | 'LWR'
+$last = null;
 
 foreach ($numbers as $n) {
 
+	switch ($next) {
+
+		case 'ANY':
+			$cur_length++;
+			$next = 'GOL';
+			break;
+
+		case 'GOL':
+			if ($n < $last) {
+				$cur_length++;
+				$next = 'GTR';
+			} elseif ($n > $last) {
+				$cur_length++;
+				$next = 'LWR';
+			} else {
+				$cur_length = 1;
+				$next = 'GOL';
+			}
+			break;
+
+		case 'GTR':
+			if ($n > $last) {
+				$cur_length++;
+				$next = 'LWR';
+			} else {
+				$cur_length = 1;
+				$next = 'GOL';
+			}
+			break;
+
+		case 'LWR':
+			if ($n < $last) {
+				$cur_length++;
+				$next = 'GTR';
+			} else {
+				$cur_length = 1;
+				$next = 'GOL';
+			}
+			break;
+
+	}
+
+	$last = $n;
+
+	if ($cur_length > $max_length) {
+		$max_length = $cur_length;
+	}
+
 }
+
+printf("Longest zigzag sub-sequence: %d\n", ($max_length >= 3) ? $max_length : 0);
